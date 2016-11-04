c6a240600c7f0bb00551c0dcbb7a4bbc9dfa9733
Herbert Jordan <herbert@dps.uibk.ac.at>
2016-11-04 10:53:26 +0100

    Correct scripting and souffle specification errors.

--- benchmarks/tak/gen_facts.sh
+++ benchmarks/tak/gen_facts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . `dirname $BASH_SOURCE[0]`/../utils.sh
 
-case $1 in
+case $SIZE in
     small)
         LOW1=-10          # number of entries
         HIGH1=10           # range of entries
@@ -12,36 +12,27 @@ case $1 in
         LOW1=-100          # number of entries
         HIGH1=100           # range of entries
         LOW2=0          # number of entries
-        HIGH2=100           # range of entries        ;;
+        HIGH2=100           # range of entries        
+        ;;
     large)
         LOW1=-1000          # number of entries
         HIGH1=1000           # range of entries
         LOW2=0          # number of entries
-        HIGH2=1000           # range of entries        ;;
+        HIGH2=1000           # range of entries        
         ;;
     xlarge)
         LOW1=-10000          # number of entries
         HIGH1=10000           # range of entries
         LOW2=0          # number of entries
-        HIGH2=10000           # range of entries        ;;
+        HIGH2=10000           # range of entries     
         ;;
 esac
 
-PROG=".decl tak(X:number, Y:number, Z:number, V:number, Q:number)
 
-tak(X,Y,Z,Y,0) :- Y >= X, natural(X), natural(Y), natural(Z).
-tak(X,Y,Z,V,Q1+Q2+Q3+Q4+1) :- tak(X-1,Y,Z,V1,Q1), tak(Y-1,Z,X,V2,Q2), tak(Z-1,X,Y,V3,Q3), 
-                              tak(V1,V2,V3,V,Q4), Y < X.
-
-.decl natural(N:number)
-
-natural(${LOW1}).
-
-natural(n+1) :- natural(n), n < {$HIGH1}.
-.decl query(N: number, Q:number) output
-
-query(N, Q) :- tak(N, 0, N+1, _, Q), N >= ${LOW2}, N < ${HIGH2}."
-
-echo $PROG > ./cprog1.dl
-
+# just seed fact files
+mkdir -p facts
+echo $LOW1 > facts/low1.facts
+echo $LOW2 > facts/low2.facts
+echo $HIGH1 > facts/high1.facts
+echo $HIGH2 > facts/high2.facts
 
--- benchmarks/turing1/gen_facts.sh
+++ benchmarks/turing1/gen_facts.sh
@@ -13,23 +13,22 @@ case $SIZE in
         N=50
         S=50          # number of entries
         A=3           # range of entries
-        L=2           # range of entries        ;;
+        L=2           # range of entries
+        ;;
     large)
         N=500
         S=500          # number of entries
         A=30           # range of entries
-        L=2           # range of entries        ;;
+        L=2           # range of entries
         ;;
     xlarge)
         N=5000
         S=5000          # number of entries
         A=30           # range of entries
-        L=2           # range of entries        ;;
+        L=2           # range of entries
         ;;
 esac
 
-echo $N
-echo $C
 
 # create fact files as needed
 #             | name | |entries| |       ranges        |
